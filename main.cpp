7ca76d0da094414156c053ff7d8097e278762d9c
Rafael Palomar <rafael.palomar@ntnu.no>
2020-08-02 21:52:03 +0200

    TASK 3: Display a triangle using shaders

--- main.cpp
+++ main.cpp
@@ -42,6 +42,9 @@ int main(void)
 
     // Create a window
     glfwWindowHint(GLFW_RESIZABLE, false);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     auto window = glfwCreateWindow(800, 600, "Hello Triangle", nullptr, nullptr);
     if (window == nullptr)
     {
@@ -67,10 +70,83 @@ int main(void)
         return EXIT_FAILURE;
     }
 
+    // Enable capture of debug output.
     glEnable(GL_DEBUG_OUTPUT);
     glDebugMessageCallback(MessageCallback, 0);
 
+    // Print OpenGL data
+    std::cout << "Vendor: " << glGetString(GL_VENDOR) << "\n";
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << "\n";
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
+
+    // Clear the background
     glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
+
+    // Create a triangle geometry
+    GLfloat triangle[3*2] = {
+    -0.5f, -0.5f,
+    0.5f, -0.5f,
+    0.0f, 0.5f
+    };
+
+    // Create a vertex array
+    GLuint vertexArrayId;
+    glGenVertexArrays(1, &vertexArrayId);
+    glBindVertexArray(vertexArrayId);
+
+    // Create a vertex buffer
+    GLuint vertexBufferId;
+    glGenBuffers(1, &vertexBufferId);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
+
+    // Populate the vertex buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, nullptr);
+    glEnableVertexAttribArray(0);
+
+
+    // Vertex shader code
+    const std::string vertexShaderSrc = R"(
+#version 430 core
+
+layout(location = 0) in vec4 position;
+
+void main()
+{
+gl_Position = position;
+}
+)";
+    // Fragment shader code
+    const std::string fragmentShaderSrc = R"(
+#version 430 core
+
+out vec4 color;
+void main()
+{
+color = vec4(1);
+}
+)";
+
+    // Compile the vertex shader
+    auto vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const GLchar* vss = vertexShaderSrc.c_str();
+    glShaderSource(vertexShader, 1, &vss, nullptr);
+    glCompileShader(vertexShader);
+
+    // Compile the fragment shader
+    auto fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const GLchar* fss = fragmentShaderSrc.c_str();
+    glShaderSource(fragmentShader, 1, &fss, nullptr);
+    glCompileShader(fragmentShader);
+
+
+    // Create a shader program
+    auto shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glUseProgram(shaderProgram);
 
     // Main loop
     while(!glfwWindowShouldClose(window))
@@ -78,6 +154,9 @@ int main(void)
         glfwPollEvents();
 
         glClear(GL_COLOR_BUFFER_BIT);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
 
